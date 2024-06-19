7e2a4edf3600929b724a4117fc20e1ff021148df
Rony Chang <56712796+RonyChang@users.noreply.github.com>
2024-06-19 10:40:47 -0500

    Update base.py

--- api/base.py
+++ api/base.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
-from api.routes import route_metric, route_pill
+from api.routes import route_metric
 
 api_router = APIRouter()
 
 
 api_router.include_router(route_metric.router, prefix="", tags=["sensors"])
-api_router.include_router(route_pill.router, prefix="", tags=["pills"])
